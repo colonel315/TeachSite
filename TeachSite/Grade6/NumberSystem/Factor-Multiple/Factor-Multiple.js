079339995d361c09c6822129a94d37ee9842c7ae
colonel315 <Fred_Burr@mymail.eku.edu>
2014-10-22 16:11:41 -0400

    added link to pages

---
+++ TeachSite/Grade6/NumberSystem/Factor-Multiple/Factor-Multiple.js
@@ -0,0 +1,9 @@
+/**
+ * Created by Trey on 10/22/2014.
+ */
+/**
+ * This function will test to see if the students have a grasp of factors, finding the GCF (through prime factorization), and th LCM (Also through Prime Factorization)
+ */
+$(document).ready(function() {
+	
+});
