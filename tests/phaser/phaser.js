293ea7679ac884066db61371289c3520452ba2d5
colonel315 <Fred_Burr@mymail.eku.edu>
2015-01-21 13:59:21 -0500

    working on questions

--- tests/phaser/phaser.js
+++ tests/phaser/phaser.js
@@ -3,14 +3,19 @@
  *
  * To do:
  *
- * Find a way to dynamically change questions, check to see if reflection, rotation and translation is all correct.
+ *  Add questions
+ *
+ *  Notes to self:
+ *
+ *  When flipping x is always 5 less from point
+ *  When Y is up it is 35 more from point
+ *  When Y is down it is 15 less from point
  */
 
 var Grid = {};
 
 Grid.Preloader = function(game) {
 	Grid.GRID_WIDTH = 800;
-
 	Grid.GRID_HEIGHT = 600;
 };
 
@@ -18,9 +23,7 @@ Grid.Preloader.prototype = {
 	preload: function() {
 		//  Load any needed images
 		this.load.image('player', 'TeachCharacter.png');
-
 		this.load.image('background', 'coordinateGrid.png');
-
 		this.load.image('transparentPoint', 'transparentPoint.png');
 	},
 
@@ -39,11 +42,6 @@ Grid.Game = function(game) {
 	//  points on a grid
 	this._transparentPointGroup = null;
 
-	//  Pointer to tell which way head is pointing
-	Grid._headUp = null;
-
-	Grid._headDown = null;
-
 	//  Displays questions on screen.
 	Grid._question = null;
 
@@ -54,9 +52,11 @@ Grid.Game = function(game) {
 	//  0 = translate, 1 = reflect
 	Grid._questionType = 0;
 
-	//  Will help to check users answer for reflect and rotate.
-	//  0 = up, 1 = down
-	Grid._headDirection = null;
+	//  Will help to check if head is up or down
+	Grid._headUp = null;
+
+	//  Will flag if key is down
+	Grid._keyDown = null;
 };
 
 Grid.Game.prototype = {
@@ -66,12 +66,10 @@ Grid.Game.prototype = {
 
 		this.add.sprite(0, 0, 'background');
 
-		Grid._headUp = false;
+		Grid._keyDown = false;
+		Grid._headUp = true;
 
-		Grid._headDown = false;
-
-		this._player = this.add.sprite(Grid.GRID_WIDTH / 2 + 2, Grid.GRID_HEIGHT / 2 + 7, 'player');
-
+		this._player = this.add.sprite(Grid.GRID_WIDTH / 2 - 5, Grid.GRID_HEIGHT / 2 + 35, 'player');
 		this._player.anchor.setTo(0.5, 1);
 
 		//this.physics.enable(this._player, Phaser.Physics.ARCADE);
@@ -94,42 +92,75 @@ Grid.Game.prototype = {
 	},
 
 	moveCharacter: function(point) {
-		this._player.x = point.x + 15;
-		this._player.y = point.y + 25;
+		if(Grid._headUp) {
+			this._player.x = point.x + 10;
+			this._player.y = point.y + 50;
+		}
+		else {
+			this._player.x = point.x + 15;
+			this._player.y = point.y;
+		}
 	},
 
 	update: function() {
-		if(this._reflectKey.isDown) {
-			this._player.scale.y *= -1;
+		if(this._reflectKey.isDown) {   //  if key is down, keyDown = true
+			Grid._keyDown = true;
+		}
+		else if(this._reflectKey.isUp) {
+			if(Grid._keyDown) { //  if keyDown is true
+				this._player.scale.y *= -1; //  flip character
 
-			Grid.Question.checkHeadDirection(this);
+				if(Grid._headUp) {  //  if head is up
+					this._player.y -= 50;   //  move character up 50 pixels
+
+					Grid._headUp = false;   //  head is not up anymore
+				}
+				else {  //  if head is down
+					this._player.y += 50;   //  move character down 50 pixels
+
+					Grid._headUp = true;    //  head is now up
+				}
+
+				Grid._keyDown = false;  //  key is not down anymore
+			}
 		}
 	}
 };
 
 Grid.Question = {
-	checkHeadDirection: function(game) {
-		if(game._player.angle >= -45 && game._player.angle <= 44) {
-			Grid._headUp = true;
-
-			Grid._headDown = false;
-
-			Grid._headDirection = 0;
-		}
-		else if((game._player.angle >= 136 && game._player.angle <= 179) || (game._player.angle >= -180 && game._player.angle <= -135)) {
-			Grid._headUp = false;
-
-			Grid._headDown = true;
-
-			Grid._headDirection = 1;
-		}
-	},
-
 	generateQuestion: function(game) {
-		switch(Grid._questionNumber) {
-			case 0:
-				game.add.text(10, 610, "");
-				break;
+		Grid._questionType = 0;
+
+		if(Grid._questionType) {    // if true will be reflection question
+			if(Math.random() >= 0.5) {  // If true will ask student to reflect character across y axis
+				if(Math.random() >= 0.5) {  //  if true will ask across y axis
+					game.add.text(10, 610, "Reflect the character across the y axis");
+				}
+				else {  // else will say horizontally
+					game.add.text(10, 610, "Reflect the character horizontally");
+				}
+			}
+			else {  //  else will tell student to reflect across x axis
+				if(Math.random() >= 0.5) {  //  if true will say x axis
+					game.add.text(10, 610, "Reflect the character across the x axis");
+				}
+				else {  //  else will say vertically
+					game.add.text(10, 610, "Reflect the character vertically");
+				}
+			}
+		}
+		else {  //  else will be translation question
+			var distanceRight = Grid.GRID_WIDTH - (game._player.x + 5);
+			var distanceLeft = Grid.GRID_WIDTH - distanceRight;
+
+			var distanceBottom;
+			if(Grid._headUp) {
+				distanceBottom = Grid.GRID_HEIGHT - (game._player.y - 35);
+			}
+			else {
+				distanceBottom = Grid.GRID_HEIGHT - (game._player.y + 15);
+				console.log("distanceBottom = ", distanceBottom);
+			}
 		}
 	},
 
